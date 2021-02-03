3d6b39e1dfbf9e084782d969a5fe2e33d82aafdd
Lix311 <alexsterrantino@gmail.com>
2021-02-02 21:48:27 -0500

    mightwork

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout'
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+          <p>Test 3</p>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
