99e4e303d7c95219e4164b548b565cdfc67dd416
Lix311 <alexsterrantino@gmail.com>
2021-02-03 18:19:22 -0500

    before css modules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout'
+import BurgerBuilder from './containers/BurgerBuilder'
 
 class App extends Component {
   render(){
     return (
       <div>
         <Layout>
-          <p>Test</p>
-          <p>Test 3</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
