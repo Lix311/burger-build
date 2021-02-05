5fb92741801f523d5999dd359ec60cb346cb17ab
Lix311 <alexsterrantino@gmail.com>
2021-02-05 16:28:57 -0500

    delete and disable

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -41,12 +41,40 @@ class BurgerBuilder extends Component {
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
 
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            return;
+        }
+        
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients}) 
+    }
+
     render() {
+        const disableInfo = {
+            ...this.state.ingredients 
+        }
+        // copies object with boolean value true/false and pass to BuildControls
+        for(let key in disableInfo){
+            disableInfo[key] = disableInfo[key] <= 0 
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}
                 />
             </Aux>
         );
